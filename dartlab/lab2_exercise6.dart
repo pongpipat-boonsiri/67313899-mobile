e502d27bfd50193bb8258b6bd73f1ea2a2f5c06f
unknown <pongpipatb67nu.ac.th>
2026-07-01 11:54:27 +0700

    Add lab2_exercise6

--- dartlab/lab2_exercise6.dart
+++ dartlab/lab2_exercise6.dart
@@ -1,16 +1,15 @@
-void greet(String name) {
-  print('Hello, $name!');
-}
-int multiply(int a, int b) => a * b;
-double calculateArea({required double width, required double height}) {
-  return width * height;
-}
+double rectangle(double w, double h) => w * h;
+double triangle(double b, double h) => 0.5 * b * h;
+double cylinder(double r, double h) => 3.14 * r * r * h;
+
 void main() {
-  greet('pongpipat');
-  int product = multiply(9, 4);
-  print('9 * 4 = $product');
-  double area = calculateArea(width: 6.7, height: 9.9);
-  print('Area (6.7 x 9.9) = $area');
-  double area2 = calculateArea(height: 4.5, width: 6.9);
-  print('Area (4.5 x 6.9) = $area2');
+  var shapes = {
+    'Rectangle': rectangle,
+    'Triangle': triangle,
+    'Cylinder': cylinder,
+  };
+
+  print('Rectangle = ${shapes['Rectangle']!(6, 4)}');
+  print('Triangle = ${shapes['Triangle']!(6, 4)}');
+  print('Cylinder = ${shapes['Cylinder']!(3, 5)}');
 }
