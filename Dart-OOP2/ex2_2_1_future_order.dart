92ad3c14e4c4f6188df26f6032ad476357b0d9d3
unknown <pongpipatb67@nu.ac.th>
2026-07-13 13:15:16 +0700

    Add Dart-OOP folder

--- Dart-OOP2/ex2_2_1_future_order.dart
+++ Dart-OOP2/ex2_2_1_future_order.dart
@@ -1,28 +1,9 @@
-Future<Map<String, dynamic>> fetchProfile(int userId) async {
-  await Future.delayed(Duration(seconds: 2));
+void main() {
+  print("1. เริ่ม");
 
-  if (userId <= 0) {
-    throw Exception("userId ไม่ถูกต้อง");
-  }
+  Future.delayed(Duration(seconds: 3)).then((_) {
+    print("2. โหลดเสร็จ");
+  });
 
-  return {
-    "id": userId,
-    "name": "pongpipat",
-    "email": "pongpipatb67@nu.ac.th",
-  };
-}
-
-Future<void> main() async {
-  try {
-    Map<String, dynamic> profile = await fetchProfile(1);
-
-    print("ข้อมูลโปรไฟล์");
-    print("ID: ${profile['id']}");
-    print("ชื่อ: ${profile['name']}");
-    print("Email: ${profile['email']}");
-  } catch (e) {
-    print("เกิดข้อผิดพลาด: $e");
-  } finally {
-    print("จบการทำงาน");
-  }
+  print("3. จบ");
 }
